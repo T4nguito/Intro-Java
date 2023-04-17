6bf1922663afa2cfe14ea58e438b7e8a3dfddfab
T4nguito <ramosjm@gmail.com>
2023-04-17 19:52:52 -0300

    Guia 1,2,3,4,5

--- Guia 5 - Arreglos en Java/src/Guia5/Ejer2.java
+++ Guia 5 - Arreglos en Java/src/Guia5/Ejer2.java
@@ -1,30 +1,38 @@
 package Guia5;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Ejer2 {
     public static void main(String[] args) {
+
         Scanner leer = new Scanner(System.in);
-        System.out.println("Ingrese la dimension del vector: ");
+        System.out.println("Ingrese la dimension del Vector: ");
         int n = leer.nextInt();
         int[] vector = new int[n];
 
+        Random numAleatorio = new Random();
         for (int i = 0; i < vector.length; i++) {
-            vector[i] = (int) (Math.random() * 10);
-                        
+            vector[i] = numAleatorio.nextInt(100 + 1);
+            /*
+             * vector[i] = (int) (Math.random() * 10);
+             * 
+             */
         }
 
-        System.outprintln(" Ingrese el numero a buscar entre 0 y 10 ");
-        int nl = leer.nextInt();
+        System.out.println(" Ingrese el numero a buscar entre 0 y 10 ");
+        int n1 = leer.nextInt();
         int cont = 0;
-        for (int i = 0; i < vector.length; i++) {
-            if (nl == vector[i]) {
-                System.out.println(" Su numero se encuentra en la posicion " + i + " del vector");
-                int 
-            }
-        }
-    
-        
-    }
 
+        for (int i = 0; i < vector.length; i++) {
+
+            if (n1 == vector[i]) {
+                System.out.println(" Su numero se encuentra en la posicion " + i + " del vector");
+                cont++;
+            }
+
+        }
+        System.out.println("La cantidad de veces que se repite su numero es " + cont);
+
+    }
 }
