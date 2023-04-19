387d6be768bebf7d3bf62e11a44bf7ebbc29c826
Mariano Ramos <ramosjr@gmail.com>
2023-04-18 21:07:26 -0300

    Guia 1,2,3,4,5

--- Guia 5 - Arreglos en Java/src/Guia5/Ejer6.java
+++ Guia 5 - Arreglos en Java/src/Guia5/Ejer6.java
@@ -14,6 +14,8 @@ public class Ejer6 {
         for (int i = 0; i < matrizA.length; i++) {
             rasultado = matrizA[i][0];
             /* sdfsdf */
+
+            /* asdasdasdsada */
         }
     }
 }
